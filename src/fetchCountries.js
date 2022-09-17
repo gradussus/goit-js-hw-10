9f7bcc306ab86f306ba143e121af355a7a0244c8
Vitalii Shevchenko <shevchenkovitaliy20@gmail.com>
2022-09-17 20:10:22 +0300

    Create fetchCountries.js

---
+++ src/fetchCountries.js
@@ -0,0 +1,7 @@
+export function fetchCountries(name) {
+  return fetch(`https://restcountries.com/v3.1/name/${name}`)
+    .then(response => {
+      return response.json();
+    })
+    .catch(error => console.log(error));
+}
